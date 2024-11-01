9d1a4aae32d89e790b3bbe276969264e6c9a066a
buhao <kiwiflydream@gmail.com>
2024-11-01 23:01:01 +0800

    fix: Replace Jsdom with Linkedom.

--- packages/server/src/utils/parse.ts
+++ packages/server/src/utils/parse.ts
@@ -1,21 +1,13 @@
 import { Readability } from '@mozilla/readability';
-import { JSDOM, VirtualConsole } from 'jsdom';
+import { parseHTML } from 'linkedom';
 import TurndownService from 'turndown'
 
 export async function getPureHtmlContent(htmlContent: string) {
   // Create virtual DOM
-  const dom = new JSDOM(htmlContent, {
-    // Disable resource loading
-    resources: "usable",
-    // Silence console warnings
-    virtualConsole: new VirtualConsole().sendTo({
-      error: () => { },
-      warn: () => { },
-    }),
-  });
+  const { document } = parseHTML(htmlContent);
 
   // Parse html to get clean HTML
-  const reader = new Readability(dom.window.document)
+  const reader = new Readability(document)
   const article = reader.parse()
   return article.content
 }
